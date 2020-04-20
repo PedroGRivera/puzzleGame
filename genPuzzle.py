b31c1c403010011b2b7386779c125c947dd9d6ad
SamP98 <samgp918@gmail.com>
2020-04-20 17:28:03 -0500

    File save/loading finished

--- genPuzzle.py
+++ genPuzzle.py
@@ -6,22 +6,31 @@ from tkinter import filedialog, Text, simpledialog
 from PIL import Image, ImageTk
 
 class Piece:
-    label = ''      #Cropped image
-    curX = ''       #Current X (row) position
-    curY = ''       #Current Y (column) position
-    finX = ''       #Final X position
-    finY = ''       #final Y position
+    def __init__(self):
+        self.label = ''
+        self.left = ''
+        self.upper = ''
+        self.right = ''
+        self.lower = ''
+        self.curX = ''       #Current X (row) position
+        self.curY = ''       #Current Y (column) position
+        self.finX = ''       #Final X position
+        self.finY = ''       #final Y position
 
 class Puzzle:
-    pieces = []     #List of pieces
-    dimension = ''  #Number of rows and columns
-    filepath = ''   #Filepath of the initial image
+    def __init__(self):
+        self.pieces = []
+        self.dimension = 0
+        self.filepath = ''
+        self.width = ''
+        self.height = ''
 
 root = tk.Tk()
 
-dimension = simpledialog.askinteger("Input", "What size grid do you want?", parent=root, minvalue = 0, maxvalue = 6)
-filepath = filedialog.askopenfilename()
+dimension = 4 #simpledialog.askinteger("Input", "What size grid do you want?", parent=root, minvalue = 0, maxvalue = 6)
+filepath = 'LSU-Logo.png' #filedialog.askopenfilename()
 
+global game
 game = Puzzle()
 game.dimension = dimension
 game.filepath = filepath
@@ -29,28 +38,28 @@ game.filepath = filepath
 #Open the starting image
 image = Image.open(game.filepath)
 
-width = image.width / game.dimension
-height = image.height / game.dimension
+game.width = image.width / game.dimension
+game.height = image.height / game.dimension
 
 def generatePuzzle(puzzle):
     print("Generating puzzle pieces")
     for i in range(puzzle.dimension): #row
         for j in range(puzzle.dimension): #column
-            tempImage = image.crop((j*width, i*height, (j+1)*width, (i+1)*height))
+            piece = Piece()
+            (piece.left, piece.upper, piece.right, piece.lower) = (j*puzzle.width, i*puzzle.height, (j+1)*puzzle.width, (i+1)*puzzle.height)
+            tempImage = image.crop((piece.left, piece.upper, piece.right, piece.lower))
             tempPhoto = ImageTk.PhotoImage(tempImage)
             label = tk.Label(master = root, image=tempPhoto, anchor=tk.CENTER)
             label.photo = tempPhoto
-            piece = Piece()
             piece.label = label
             piece.finX = i
             piece.finY = j
-            print(str(piece.finX) + "," + str(piece.finY))
             puzzle.pieces.append(piece)
 
 
 def scramblePuzzle(puzzle):
     print("Shuffling puzzle pieces")
-    random.shuffle(puzzle) #Pieces shuffled
+    random.shuffle(puzzle.pieces) #Pieces shuffled
     #Update current positions
     for i in range(puzzle.dimension):
         for j in range(puzzle.dimension):
@@ -60,18 +69,26 @@ def scramblePuzzle(puzzle):
 
 def displayPuzzle(puzzle):
     print("Displaying puzzle pieces")
-    for i in range(puzzle.dimension):
-        for j in range(puzzle.dimension):
-            puzzle.pieces[(puzzle.dimension)*i + j].label.place(x=j*(width), y=i*(height))
+    # for i in range(puzzle.dimension):
+    #     for j in range(puzzle.dimension):
+    #         puzzle.pieces[(puzzle.dimension)*i + j].label.place(x=j*(width), y=i*(height))
 
-# def savePuzzle(puzzle):
-#     print("Saving data...")
-#     pickle.dump(puzzle, str(puzzle.filepath) + ".save")
-#     print("Data saved to " + str(puzzle.filepath) + ".save")
+    for i in range (len(puzzle.pieces)):
+        puzzle.pieces[i].label.place(x = puzzle.pieces[i].curY*puzzle.width, y=puzzle.pieces[i].curX*puzzle.height)
 
-#generatePuzzle(game)
-#scramblePuzzle(game)
-#displayPuzzle(game)
-#savePuzzle(game)
+def savePuzzle(puzzle):
+    print("Saving data...")
+
+    for i in range(len(puzzle.pieces)): #clear out pictures since they can't be pickled
+        puzzle.pieces[i].label = ''
+
+    file_pi = open(filepath + ".puz", 'wb')
+    pickle.dump(puzzle, file_pi)
+    print("Data saved")
+
+generatePuzzle(game)
+scramblePuzzle(game)
+displayPuzzle(game)
+savePuzzle(game)
 
 root.mainloop()
--- openSaveTest.py
+++ openSaveTest.py
@@ -7,15 +7,50 @@ from tkinter import filedialog, Text, simpledialog
 from PIL import Image, ImageTk
 
 class Piece:
-    label = ''      #Cropped image
-    curX = ''       #Current X (row) position
-    curY = ''       #Current Y (column) position
-    finX = ''       #Final X position
-    finY = ''       #final Y position
+    def __init__(self):
+        self.label = ''
+        self.left = ''
+        self.upper = ''
+        self.right = ''
+        self.lower = ''
+        self.curX = ''       #Current X (row) position
+        self.curY = ''       #Current Y (column) position
+        self.finX = ''       #Final X position
+        self.finY = ''       #final Y position
 
 class Puzzle:
-    pieces = []     #List of pieces
-    dimension = ''  #Number of rows and columns
-    filepath = ''   #Filepath of the initial image
+    def __init__(self):
+        self.pieces = []
+        self.dimension = 0
+        self.filepath = ''
 
-game = pickle.load()
+def displayPuzzle(puzzle):
+    print("Displaying puzzle pieces")
+
+    for i in range (len(puzzle.pieces)):
+        puzzle.pieces[i].label.place(x = puzzle.pieces[i].curY*puzzle.width, y=puzzle.pieces[i].curX*puzzle.height) 
+
+def loadPuzzle():
+    file_pi = open(filepath + ".puz", 'rb')
+    global puzzle 
+    puzzle = pickle.load(file_pi)
+    image = Image.open(puzzle.filepath)
+
+    for i in range(len(puzzle.pieces)): #remake pictures from dimensions
+        tempImage = image.crop((puzzle.pieces[i].left, puzzle.pieces[i].upper, puzzle.pieces[i].right, puzzle.pieces[i].lower))
+        tempPhoto = ImageTk.PhotoImage(tempImage)
+        label = tk.Label(master = root, image=tempPhoto, anchor=tk.CENTER)
+        label.photo = tempPhoto
+        puzzle.pieces[i].label = label
+
+    print("Puzzle save data opened")
+
+root = tk.Tk()
+
+filepath = 'LSU-Logo.png'
+
+loadPuzzle()
+
+displayPuzzle(puzzle)
+
+root.mainloop()
