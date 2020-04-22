797ded3640a7b44d15622238f14dbfe7160de15d
SamP98 <samgp918@gmail.com>
2020-04-22 11:46:23 -0500

    Save/load data working.

--- genPuzzle.py
+++ genPuzzle.py
@@ -7,8 +7,8 @@ from PIL import Image, ImageTk
 
 class Piece:
     def __init__(self):
-        self.label = ''
-        self.left = ''
+        self.label = ''     #label holds the puzzle piece itself, may need to change this to button if we use buttons later
+        self.left = ''      #left, upper, right, lower are needed to crop the piece
         self.upper = ''
         self.right = ''
         self.lower = ''
@@ -19,29 +19,26 @@ class Piece:
 
 class Puzzle:
     def __init__(self):
-        self.pieces = []
-        self.dimension = 0
-        self.filepath = ''
-        self.width = ''
+        self.pieces = []    #list of all puzzle pieces
+        self.dimension = 0  #puzzle dimensions set by the user
+        self.filepath = ''  #location of the base image
+        self.width = ''     #widht and height, needed to generate piece dimensions
         self.height = ''
 
 root = tk.Tk()
 
-dimension = 4 #simpledialog.askinteger("Input", "What size grid do you want?", parent=root, minvalue = 0, maxvalue = 6)
-filepath = 'LSU-Logo.png' #filedialog.askopenfilename()
-
 global game
 game = Puzzle()
-game.dimension = dimension
-game.filepath = filepath
-
-#Open the starting image
-image = Image.open(game.filepath)
-
-game.width = image.width / game.dimension
-game.height = image.height / game.dimension
 
 def generatePuzzle(puzzle):
+    puzzle.dimension = simpledialog.askinteger("Input", "What size grid do you want?", parent=root, minvalue = 0, maxvalue = 6)
+    puzzle.filepath = filedialog.askopenfilename()
+    image = Image.open(puzzle.filepath)
+    puzzle.width = image.width / puzzle.dimension
+    puzzle.height = image.height / puzzle.dimension
+    generatePieces(puzzle, image)
+
+def generatePieces(puzzle, image):
     print("Generating puzzle pieces")
     for i in range(puzzle.dimension): #row
         for j in range(puzzle.dimension): #column
@@ -53,7 +50,9 @@ def generatePuzzle(puzzle):
             label.photo = tempPhoto
             piece.label = label
             piece.finX = i
+            piece.curX = i
             piece.finY = j
+            piece.curY = j
             puzzle.pieces.append(piece)
 
 
@@ -66,26 +65,22 @@ def scramblePuzzle(puzzle):
             puzzle.pieces[(puzzle.dimension)*i + j].curX = i
             puzzle.pieces[(puzzle.dimension)*i + j].curY = j
 
-
+#This could use some work, some pieces kinda clip each other
 def displayPuzzle(puzzle):
     print("Displaying puzzle pieces")
-    # for i in range(puzzle.dimension):
-    #     for j in range(puzzle.dimension):
-    #         puzzle.pieces[(puzzle.dimension)*i + j].label.place(x=j*(width), y=i*(height))
-
     for i in range (len(puzzle.pieces)):
         puzzle.pieces[i].label.place(x = puzzle.pieces[i].curY*puzzle.width, y=puzzle.pieces[i].curX*puzzle.height)
 
 def savePuzzle(puzzle):
     print("Saving data...")
-
     for i in range(len(puzzle.pieces)): #clear out pictures since they can't be pickled
         puzzle.pieces[i].label = ''
-
-    file_pi = open(filepath + ".puz", 'wb')
+    file_pi = open(puzzle.filepath + ".puz", 'wb')
     pickle.dump(puzzle, file_pi)
     print("Data saved")
 
+
+#Function calls to make sure the above all works
 generatePuzzle(game)
 scramblePuzzle(game)
 displayPuzzle(game)
